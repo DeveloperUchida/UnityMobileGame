8c969f8da39bc874f0fcd536bfdbc5c34eb59c2d
DeveloperUchida <kouei28poiou@icloud.com>
2024-10-10 15:37:05 +0900

    SorceCodeUpdate

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float PlayerSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, PlayerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-PlayerSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -PlayerSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(PlayerSpeed * Time.deltaTime,0,0);
+        }
+    }
+}
