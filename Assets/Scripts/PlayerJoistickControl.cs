6564cb5a19ee123d5886d497e0027e29c9482ab0
DeveloperUchida <kouei28poiou@icloud.com>
2024-11-20 10:56:51 +0900

    DataUpdate

--- Assets/Scripts/PlayerJoistickControl.cs
+++ Assets/Scripts/PlayerJoistickControl.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerJoistickControl : MonoBehaviour
 {
+    private float PlayerSpeed = 10;
+    private Rigidbody PlayerRigid; //プレイヤーのRigidBodyの名前を PlayerRigidへ
+    
+    private Vector2 movement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PlayerRigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
